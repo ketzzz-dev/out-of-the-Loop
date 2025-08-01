718fbfddc76ecf09b151e1c3d9dbe29a6b170562
Pasas1345 <jamesban31@gmail.com>
2025-08-02 00:37:06 +0800

    I GOT IT TO WORK

--- Assets/Scripts/Items/Item.cs
+++ Assets/Scripts/Items/Item.cs
@@ -1,10 +1,32 @@
+using MADCUP.STM;
 using UnityEngine;
 
 public enum ItemType {
-	Placeholder
+	None,
+	Placeholder,
+	Placeholder2,
+	// Rest of the other items here
 }
 
-public class Item : MonoBehaviour
-{
+public class Item : MonoBehaviour {
 	public ItemType itemType;
+	public Sprite itemSprite;
+	public bool initalized;
+	[SerializeField] private SpriteMesh spriteMesh;
+
+	private void Start() {
+		if (itemType != ItemType.None) {
+			spriteMesh.sprite = itemSprite;
+			initalized = true;
+		}
+	}
+
+	// This is used for dropped items.
+	public void Initialize(Sprite sprite, ItemType type) {
+		initalized = true;
+		itemSprite = sprite;
+		itemType = type;
+
+		spriteMesh.sprite = sprite;
+	}
 }
