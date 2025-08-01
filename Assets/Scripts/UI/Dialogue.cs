cb47dddb02d441b69b053c803931db914c9d78bb
Keys <0hlowkey750@gmail.com>
2025-08-01 23:58:26 +0800

    Safety Checks

--- Assets/Scripts/UI/Dialogue.cs
+++ Assets/Scripts/UI/Dialogue.cs
@@ -1,17 +1,22 @@
 using System.Collections;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Dialogue : MonoBehaviour
 {
+    [Header("Text")]
     [SerializeField] private TextMeshProUGUI subtitles;
     [SerializeField] private float typingRate;
 
+    [Header("Options")]
     [SerializeField] private GameObject options;
     [SerializeField] private GameObject optionPrefab;
+    [SerializeField] private int maxOptions = 4;
 
     private bool isTyping;
+    private List<GameObject> optionPool = new List<GameObject>();
 
     private void Start()
     {
@@ -23,6 +28,29 @@ public class Dialogue : MonoBehaviour
 
         DialogueManager.instance.onSelectionStarted += OnSelectionStarted;
         DialogueManager.instance.onSelectionEnded += OnSelectionEnded;
+
+        for(int i = 0; i < maxOptions; i++)
+        {
+            var option = Instantiate(optionPrefab, options.transform);
+
+            option.SetActive(false);
+            optionPool.Add(option);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (DialogueManager.instance == null)
+        {
+            return;
+        }
+        
+        DialogueManager.instance.onDialogueStarted -= OnDialogueStarted;
+        DialogueManager.instance.onDialogueChanged -= OnDialogueChanged;
+        DialogueManager.instance.onDialogueEnded -= OnDialogueEnded;
+
+        DialogueManager.instance.onSelectionStarted -= OnSelectionStarted;
+        DialogueManager.instance.onSelectionEnded -= OnSelectionEnded;
     }
 
     private void Update()
@@ -31,7 +59,10 @@ public class Dialogue : MonoBehaviour
         {
             if (isTyping)
             {
+                StopAllCoroutines();
+
                 subtitles.maxVisibleCharacters = subtitles.text.Length;
+                isTyping = false;
 
                 return;
             }
@@ -44,6 +75,11 @@ public class Dialogue : MonoBehaviour
 
     private void OnDialogueStarted(string content)
     {
+        if (subtitles == null)
+        {
+            return;
+        }
+
         subtitles.enabled = true;
 
         ShowDialogue(content);
@@ -61,29 +97,61 @@ public class Dialogue : MonoBehaviour
 
     private void OnSelectionStarted(string[] labels)
     {
-        foreach (var id in labels)
+        if (optionPrefab == null || options == null)
         {
-            var option = Instantiate(optionPrefab, options.transform);
-            var button = option.GetComponent<Button>();
-            var label = option.GetComponentInChildren<TextMeshProUGUI>();
+            Debug.LogError("UI references not set");
 
-            button.onClick.AddListener(() =>
+            return;
+        }
+
+        for (int i = 0; i < labels.Length; i++)
+        {
+            if (i >= maxOptions)
             {
-                DialogueManager.instance.SelectBranch(id);
-            });
+                break;
+            };
+            
+            var option = optionPool[i];
 
-            label.text = id;
+            option.SetActive(true);
+            
+            var button = option.GetComponent<Button>();
+            var labelText = option.GetComponentInChildren<TextMeshProUGUI>();
+
+            if (labelText)
+            {
+                labelText.text = labels[i];
+            }
+            
+            string currentLabel = labels[i];
+
+            button.onClick.RemoveAllListeners();
+            button.onClick.AddListener(() => 
+            {
+                DialogueManager.instance.SelectBranch(currentLabel);
+            });
         }
     }
     private void OnSelectionEnded()
     {
-        foreach (Transform child in options.transform) {
-            Destroy(child.gameObject);
+        options.SetActive(false);
+
+        foreach (var option in optionPool)
+        {
+            option.SetActive(false);
         }
     }
 
     private void ShowDialogue(string content)
     {
+        if (subtitles == null)
+        {
+            return;
+        }
+
+        StopAllCoroutines();
+
+        isTyping = false;
         subtitles.text = content;
         subtitles.maxVisibleCharacters = 0;
 
