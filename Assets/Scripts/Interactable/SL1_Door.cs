1bb1abc3caa71793421be2d1a85234e05967314c
BooleanAnthony <julanthony93@gmail.com>
2025-08-02 16:15:18 +0800

    added lock and key system

---
+++ Assets/Scripts/Interactable/SL1_Door.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+public class SL1_Door : Interactable
+{
+    //change header and field for other key&locks i guess
+    [Header("Door Settings")]
+    [SerializeField] private string requiredKey = "door key"; //holy shit please make sure its the same name as the component dont be like me 
+    private bool isOpen = false;
+
+    void OnMouseDown()  
+    {
+        if (isOpen)
+        {
+            Debug.Log("Door is already unlocked.");
+            return;
+        }
+
+        if (Inventory.instance.heldItem == null)
+        {
+            Debug.Log("You're not holding any item.");
+            return;
+        }
+
+        if (Inventory.instance.heldItem.name == requiredKey && !isOpen)
+        {
+            OpenDoor();
+        }
+        else
+        {
+            Debug.Log("This item can't unlock the door.");
+        }
+    }
+
+    private void OpenDoor()
+    {
+        isOpen = true;
+        Debug.Log("Door unlocked!");
+
+        //rotates like a door
+        transform.Rotate(0, -90, 0);
+
+        //use up item
+        Inventory.instance.ClearHeldItem();
+    }
+
+    public override void Interact()
+    {
+        if (!isOpen)
+        {
+            //shows dialogue if its still locked
+            base.Interact();
+        }
+        else
+        {
+            Debug.Log("No dialogue triggered because door is open.");
+        }
+    }
+}
